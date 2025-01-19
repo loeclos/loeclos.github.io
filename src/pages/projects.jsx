230a4362c5b27966114f7b78c852d4732da95f0e
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2025-01-18 18:53:23 -0800

    Change skills

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -101,7 +101,6 @@ const projects = [
         imageSrc:
             'https://github.com/marsianjohncarter/github-react-example/assets/116607327/caed8b99-c1aa-4cb6-8114-e24fc87aa37d',
     },
-    ,
     {
         id: 5,
         title: 'Nutrify Website',
