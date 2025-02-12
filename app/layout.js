29eb75d1b254f4a593b77f957055e1c0a84d96b0
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2025-02-12 15:47:08 -0800

    Add style for mdeia darkmode

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ const links = [
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" className="light">
+        <html lang="en">
             <body className={`${geistMono.className} antialiased`}>
                 <div className="fixed top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] hidden dark:block"></div>
                 <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] block dark:hidden"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    darkMode: 'class', 
+    darkMode: 'media', 
     variants: {
       scrollbar: ['rounded'], 
     },
