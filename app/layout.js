09cbfd119d237d5406fc1e9b652d2988be6cb6ab
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2025-02-19 11:06:38 -0800

    Add google analytics

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import {
     montserrat,
     grenzeGotisch,
 } from '@/ui/lib/fonts';
+import Script from 'next/script';
 import Navbar from '@/components/navbar/navbar';
 import Footer from '@/components/footer/footer';
 import ThemeProvider from '@/ui/theme/theme-provider';
@@ -26,8 +27,24 @@ const links = [
 export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className={`${geistMono.className} antialiased transition-all delay-150 duration-300 `}>
-                <ThemeProvider attribute="class" defaultTheme="light" storageKey="theme">
+            <Script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-NHZ14B3DLH"
+            ></Script>
+            <Script>
+                {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
+            </Script>
+            <body
+                className={`${geistMono.className} antialiased transition-all delay-150 duration-300 `}
+            >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="light"
+                    storageKey="theme"
+                >
                     <div className="fixed top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] hidden dark:block"></div>
                     <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] block dark:hidden"></div>
                     <Navbar links={links} />
